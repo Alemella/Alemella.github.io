5a6a74896c60383273a56215ff6dc2cbbecda1b1
Alemella <alejandros.mella5@gmail.com>
2026-02-28 21:00:47 -0300

    CAMBIOS

---
+++ my-react-app/src/components/ExampleComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ExampleComponent = () => {
+    return (
+        <div className="example-component">
+            <h2>Example Component</h2>
+            <p>This is an example of a functional component in React.</p>
+        </div>
+    );
+};
+
+export default ExampleComponent;
